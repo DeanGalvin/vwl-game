b5b9764810de41420e13e1c32c64c335f35f7142
Dean <deangalvin10@gmail.com>
2026-03-12 20:11:11 +0000

    Fix Vite base path for Github Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vwl-game/', // Tells Vite to use relative paths for GitHub Pages subdirectories
 })
